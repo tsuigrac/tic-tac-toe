83cff2dc7afe181a2166028b14fe8b75e10c9d37
tsuigrac <tsuigrac@grinnell.edu>
2020-05-21 23:20:46 -0500

    Two loops to render board

--- src/index.js
+++ src/index.js
@@ -22,23 +22,18 @@ class Board extends React.Component {
     }
 
     render() {
+        let boardSquares = [];
+        for(let row = 0; row < 3; row++) {
+            let rowSquares = [];
+            for(let col = 0; col < 3; col++) {
+                rowSquares.push(this.renderSquare(row*3 + col));
+            }
+            boardSquares.push(<div className="board-row">{rowSquares}</div>);
+        }
+        
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {boardSquares}
             </div>
         );
     }
